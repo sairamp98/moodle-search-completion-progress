ea48281506120a02fcef82807be96adc156916a0
sairamp98 <sri.sai.ram.pothuri-1@ou.edu>
2024-08-22 18:09:05 -0500

    modifies completion plugin with a search functionality based on last names

---
+++ lang/en/report_cpm_completion.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package    report
+ * @subpackage cpm_completion
+ * @author     Sri Sairam Pothuri <sri.sai.ram.pothuri-1@ou.edu>
+ */
+
+$string['clicktomarkusercomplete'] = 'Click to mark user complete';
+$string['cpm_completion:view'] = 'View course completion report';
+$string['completiondate'] = 'Completion date';
+$string['id'] = 'ID';
+$string['name'] = 'Name';
+$string['nocapability'] = 'Can not access user completion report';
+$string['page-report-completion-x'] = 'Any completion report';
+$string['page-report-completion-index'] = 'Course completion report';
+$string['page-report-completion-user'] = 'User course completion report';
+$string['pluginname'] = 'Rft Completion';
+$string['privacy:metadata'] = 'The Course completion report only shows data stored in other locations.';
+$string['eventreportviewed'] = 'Completion report viewed';
+$string['eventuserreportviewed'] = 'Completion user report viewed';
+$string['searchuser'] = 'Search by last name';
+
